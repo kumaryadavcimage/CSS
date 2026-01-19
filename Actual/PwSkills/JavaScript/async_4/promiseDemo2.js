261017de74b26a172f21367e40e26d8d4ddf7aa9
kumaryadavcimage <kumaryadav9858845426@gmail.com>
2026-01-19 10:40:14 +0530

    Added codes

---
+++ Actual/PwSkills/JavaScript/async_4/promiseDemo2.js
@@ -0,0 +1,13 @@
+function createSyncPromise () {
+    return new Promise((res,res) => {
+        // sync algorithm
+        for (let i = 0; i < 10000000; i++) {
+            // some sync work
+            
+        }
+        res("sync work is done",100)
+    })
+}
+
+const response = createSyncPromise();
+console.log("response is ",response)
